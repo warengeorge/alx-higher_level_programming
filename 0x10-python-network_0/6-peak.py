7b4e17a36e81600a72e760a73a6d27405fa4ff84
warengeorge <georgeomiebi@gmail.com>
2023-01-17 11:16:58 +0100

    Fix: effiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-""" This module returns the peak of the list
-"""
+"""Algorithms for list of integers."""
 
 
 def find_peak(list_of_integers):
-    """ This function returns the peak of the list
-    """
-    if (len(list_of_integers) == 0):
-        return None
-
-    else:
-        peak = list_of_integers[0]
-        for i in range(len(list_of_integers)):
-            if list_of_integers[i] > peak:
-                peak = list_of_integers[i]
-        return peak
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
